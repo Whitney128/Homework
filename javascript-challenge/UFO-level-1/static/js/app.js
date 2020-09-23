cfb649682b77be7fcb49bb25c29ec97b4fe5d421
Whitney <White.Whitney543@gmail.com>
2020-09-22 18:10:06 -0600

    good

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -3,6 +3,7 @@ var tableData = data;
 console.log(tableData);
 
 var tbody = d3.select("tbody");
+//form.on("submit", runEnter);
 
 tableData.forEach(function(ufosighting) {
 	console.log(ufosighting);
@@ -10,9 +11,56 @@ tableData.forEach(function(ufosighting) {
 
 	Object.entries(ufosighting).forEach(function([key, value]) {
 		console.log(key, value);
-		var cell = row.append("td");
+		var cell = row.append("td")
 		cell.text(value);
 	});
 });
+
+//search bar
+var button = d3.select("#button");
+button.on("click", runEnter);
+
+function runEnter() {
+
+	//d3.event.preventDefault();
+	tbody.html('');
+
+	var inputelement = d3.select("#datetime");
+	var inputvalue = inputelement.property("value");
+	console.log(inputvalue);
+	//console.log(ufosighting);
+
+	filteredData = tableData.filter(ufosighting => ufosighting.datetime === inputvalue);
+	console.log(filteredData);
+
+	filteredData.forEach(function(ufosighting) {
+		console.log(ufosighting);
+		var row = tbody.append("tr");
+
+		Object.entries(ufosighting).forEach(function([key, value]) {
+			console.log(key, value);
+			var cell = row.append("td")
+			cell.text(value);
+		});
+	});
+};
+
+// var tbody = d3.select("tbody");
+
+
+// 	//tbody.selectall("tr").data(filteredData).enter().append("tr");
+// 	filteredData.forEach(function(ufosighting) {
+// 	console.log(ufosighting);
+// 	var row = tbody.append("tr");
+
+// 	Object.entries(ufosighting).forEach(function([key, value]) {
+// 		console.log(key, value);
+// 		var cell = row.append("td")
+// 		cell.text(value);
+// 	});
+// });
+
+
+
 
 // YOUR CODE HERE!
--- javascript-challenge/UFO-level-2/static/js/app.js
+++ javascript-challenge/UFO-level-2/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
+var tbody = d3.select("tbody");
+//form.on("submit", runEnter);
+
+tableData.forEach(function(ufosighting) {
+	console.log(ufosighting);
+	var row = tbody.append("tr");
+
+	Object.entries(ufosighting).forEach(function([key, value]) {
+		console.log(key, value);
+		var cell = row.append("td")
+		cell.text(value);
+	});
+});
+
+var button = d3.select("#button");
+button.on("click", runEnter);
 // YOUR CODE HERE!
